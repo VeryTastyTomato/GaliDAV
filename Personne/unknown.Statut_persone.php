4a3bc1b1b1d6cf2d70c94e0ac6acd20e18ebb12c
Orni2Thorinque <Orni2Thorinque@gmail.com>
2015-03-21 23:46:29 +0100

    Mise en anglais
    traduction douteuse de intervenant et responsable

--- Personne/unknown.Statut_persone.php
+++ Personne/unknown.Statut_persone.php
@@ -2,36 +2,18 @@
 
 error_reporting(E_ALL);
 
-/**
- * modèle.edt.galilée - Personne/unknown.Statut_persone.php
- *
- * $Id$
- *
- * This file is part of modèle.edt.galilée.
- *
- * Automatically generated on 18.03.2015, 17:39:37 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- * @package Personne
- */
-
 if (0 > version_compare(PHP_VERSION, '5')) {
     die('This file was generated for PHP 5');
 }
 
-/* user defined includes */
-// section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000BA7-includes begin
-// section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000BA7-includes end
-
 /* user defined constants */
-define("ETUDIANT", 0);
-define("INTERVENANT", 1);
-define("ENSEIGNANT", 2);
-define("SECRETAIRE", 3);
-define("RESPONSABLE", 4);
-define("ADMINISTRATEUR", 5);
-define("AUTRE", 6);
+define("STUDENT", 0);
+define("SPEAKER", 1);
+define("TEACHER", 2);
+define("SECREATARY", 3);
+define("PERSON_IN_CHARGE", 4);
+define("ADMINISTRATOR", 5);
+define("OTHER", 6);
 
 null
 
