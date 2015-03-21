c26f9df49ff7667ab5db636a44597a93ddf0e6f3
VeryTastyTomato <Link.i@hotmail.fr>
2015-03-21 18:58:16 +0100

    Nettoyage et mise en forme

--- class.Classe.php
+++ class.Classe.php
@@ -1,100 +1,29 @@
 <?php
-
 error_reporting(E_ALL);
 
-/**
- * modèle.edt.galilée - class.Classe.php
- *
- * $Id$
- *
- * This file is part of modèle.edt.galilée.
- *
- * Automatically generated on 18.03.2015, 17:41:28 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
+if (0 > version_compare(PHP_VERSION, '5'))
+{
+	die('This file was generated for PHP 5');
 }
 
-/**
- * include EDTClasse
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.EDTClasse.php');
-
-/**
- * include Groupe
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.Groupe.php');
-
-/**
- * include Maquette
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.Maquette.php');
-
-/**
- * include Personne
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.Personne.php');
-
-/**
- * include Responsable
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.Responsable.php');
 
-/* user defined includes */
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD2-includes begin
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD2-includes end
-
-/* user defined constants */
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD2-constants begin
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD2-constants end
-
-/**
- * Short description of class Classe
- *
- * @access public
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
-class Classe
-    extends Groupe
+class Classe extends Groupe
 {
-    // --- ASSOCIATIONS ---
-    // generateAssociationEnd :     // generateAssociationEnd :     // generateAssociationEnd :     // generateAssociationEnd : responsables    // generateAssociationEnd : 
+	// --- ASSOCIATIONS ---
 
-    // --- ATTRIBUTES ---
+	// --- ATTRIBUTES ---
 
-    // --- OPERATIONS ---
+	// --- OPERATIONS ---
+	public function getEDTClasse()
+	{
+		$returnValue = null;
 
-    /**
-     * Short description of method getEDTclasse
-     *
-     * @access public
-     * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
-     * @return EDTClasse
-     */
-    public function getEDTClasse()
-    {
-        $returnValue = null;
-
-        // section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD3 begin
-        // section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000BD3 end
-
-        return $returnValue;
-    }
-
-} /* end of class Classe */
-
+		return $returnValue;
+	}
+}
 ?>
--- class.Cours.php
+++ class.Cours.php
@@ -1,103 +1,30 @@
 <?php
-
 error_reporting(E_ALL);
 
-/**
- * modèle.edt.galilée - class.Cours.php
- *
- * $Id$
- *
- * This file is part of modèle.edt.galilée.
- *
- * Automatically generated on 18.03.2015, 17:40:36 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
+if (0 > version_compare(PHP_VERSION, '5'))
+{
+	die('This file was generated for PHP 5');
 }
 
-/**
- * include Cours_Type_cours
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('Cours/unknown.Type_cours.php');
-
-/**
- * include EDT
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.EDT.php');
-
-/**
- * include Matiere
- *
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 require_once('class.Matiere.php');
 
-/* user defined includes */
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000C34-includes begin
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000C34-includes end
-
-/* user defined constants */
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000C34-constants begin
-// section 127-0-1-1--c7f407:14ba118f2a2:-8000:0000000000000C34-constants end
-
-/**
- * Short description of class Cours
- *
- * @access public
- * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
- */
 class Cours
 {
-    // --- ASSOCIATIONS ---
-    // generateAssociationEnd : listeEDTs    // generateAssociationEnd :     // generateAssociationEnd :     // generateAssociationEnd : 
+	// --- ASSOCIATIONS ---
 
-    // --- ATTRIBUTES ---
-    /**
-     * Short description of attribute Numero
-     *
-     * @access public
-     * @var Integer
-     */
-    public $number = null;
+	// --- ATTRIBUTES ---
+	private $number = null;
+	private $begin[null | null | null];
+	private $end[null | null | null];
+	private $room = null;
+	private $typeCours = null;
+	private $subject = null; //Etienne : la matière a sa place dans les attributs non ?<-Flora : OUi, c'est d'ailleurs une association vers la classe Matière
 
-    /**
-     * Short description of attribute Début
-     *
-     * @access public
-     */
-    public $begin[ null | null | null ];
+	// --- OPERATIONS ---
 
-    /**
-     * Short description of attribute Fin
-     *
-     * @access public
-     */
-    public $end[ null | null | null ];
-
-    /**
-     * Short description of attribute Salle
-     *
-     * @access public
-     * @var String
-     */
-    public $room = null;
-
-	public $typeCours = null;
-
-	public $subject = null; //Etienne : la matière a sa place dans les attributs non ?<-Flora : OUi, c'est d'ailleurs une association vers la classe Matière
-
-    // --- OPERATIONS ---
-
-	//getters
-
+	// getters
 	public function getNumber()
 	{
 		return $this->number;
@@ -112,7 +39,7 @@ class Cours
 	{
 		return $this->end;
 	}
-	
+
 	public function getRoom()
 	{
 		return $this->room;
@@ -123,11 +50,10 @@ class Cours
 		return $this->subject;
 	}
 
-	//setters
-
+	// setters
 	public function setNumber($newNumber)
 	{
-		if(!empty($newNumber))
+		if (!empty($newNumber))
 		{
 			$this->number = $newNumber;
 		}
@@ -135,7 +61,7 @@ class Cours
 
 	public function setBegin($newBegin)
 	{
-		if(!empty($newBegin))
+		if (!empty($newBegin))
 		{
 			$this->begin = $newBegin;
 		}
@@ -143,7 +69,7 @@ class Cours
 
 	public function setEnd($newEnd)
 	{
-		if(!empty($newEnd))
+		if (!empty($newEnd))
 		{
 			$this->end = $newEnd;
 		}
@@ -151,7 +77,7 @@ class Cours
 
 	public function setRoom($newRoom)
 	{
-		if(!empty($newRoom))
+		if (!empty($newRoom))
 		{
 			$this->room = $newRoom;
 		}
@@ -159,79 +85,26 @@ class Cours
 
 	public function setSubject($newSubject)
 	{
-		if(!empty($newSubject))
+		if (!empty($newSubject))
 		{
 			$this->subject = $newSubject;
 		}
 	}
 
-    /**
-     * Short description of method retirer
-     *
-     * @access public
-     * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
-     * @return mixed
-     */
-    public function retire()
-    {
-	//Etienne : accès à la BDD pour la delete ?
+	// others
+	public function remove()
+	{
+		//Etienne : accès à la BDD pour la delete ?
+	}
 
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C04 begin
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C04 end
-    }
+	public function Cours(Matiere $M, $Debut, $Fin)
+	{
+		// Etienne : c'est un constructeur ça ?
+		// Ibrar : il semblerait, le cas échéant il y a des méthodes standards pour ça
+	}
 
-    /**
-     * Short description of method modifierSalle
-     *
-     * @access public
-     * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
-     * @param  String S
-     * @return mixed
-     */
-    public function modifyRoom( String $S)
-    {
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C06 begin
-
-	if(!empty($S))
-		{
-			$this->room = $S;
-		}
-	//Etienne : c'est une sorte de setteur ça...
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C06 end
-    }
-
-    /**
-     * Short description of method Cours
-     *
-     * @access public
-     * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
-     * @param  Matiere M
-     * @param  Debut
-     * @param  Fin
-     * @return mixed
-     */
-    public function Cours( Matiere $M, $Debut, $Fin)
-    {
-	// Etienne : c'est un constructeur ça ?
-
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C09 begin
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C09 end
-    }
-
-    /**
-     * Short description of method integrerDansEDT
-     *
-     * @access public
-     * @author Flora KAPGNEP, <flora.kapgnep@gmail.com>
-     * @param  EDT E
-     * @return mixed
-     */
-    public function integrerDansEDT( EDT $E)
-    {
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C0E begin
-        // section 127-0-1-1--3a776dd5:14ba843849d:-8000:0000000000000C0E end
-    }
-
-} /* end of class Cours */
-
+	public function integrateInTimetable(EDT $timetable)
+	{
+	}
+}
 ?>
