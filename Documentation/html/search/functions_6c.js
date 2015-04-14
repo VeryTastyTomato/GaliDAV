645bad8fafdf99cf68bda79f20394e204280db1c
VeryTastyTomato <Link.i@hotmail.fr>
2015-04-14 15:06:08 +0200

    Mise à jour doc

--- Documentation/html/search/functions_6c.js
+++ Documentation/html/search/functions_6c.js
@@ -3,12 +3,10 @@ var searchData=
   ['loadcoursefromressource',['loadCourseFromRessource',['../class_e_d_t.html#abe93d63108b754bc99754ef32a2bf86e',1,'EDT']]],
   ['loadfromdb',['loadFromDB',['../class_classe.html#a9440622459c850bfa26923fb6f854fff',1,'Classe\loadFromDB()'],['../class_e_d_t.html#a2f014444c5fde45b477dccfd4ae1a63f',1,'EDT\loadFromDB()'],['../class_groupe.html#aae3d552e670c755062a30caa7f152284',1,'Groupe\loadFromDB()'],['../class_matiere.html#af1f8298dc4200130c1627836771601d5',1,'Matiere\loadFromDB()'],['../class_personne.html#ae3d46de451bf7c7234bcae2818e61086',1,'Personne\loadFromDB()'],['../class_utilisateur.html#aa0f776b3efd082e2786307cf9b77d0e6',1,'Utilisateur\loadFromDB()']]],
   ['loadfromressource',['loadFromRessource',['../class_e_d_t.html#a3a53d2c42489ff72eead988ddfeaa8de',1,'EDT\loadFromRessource()'],['../class_groupe.html#a3a53d2c42489ff72eead988ddfeaa8de',1,'Groupe\loadFromRessource()'],['../class_matiere.html#a3a53d2c42489ff72eead988ddfeaa8de',1,'Matiere\loadFromRessource()'],['../class_personne.html#a3a53d2c42489ff72eead988ddfeaa8de',1,'Personne\loadFromRessource()']]],
-  ['loadgroupfromressource',['loadGroupFromRessource',['../class_matiere.html#a3c8d7064c2603335bf49b3cd5dfe91ae',1,'Matiere']]],
   ['loadlinkedgroupfromressource',['loadLinkedGroupFromRessource',['../class_groupe.html#a4501c3cef9b24dce2abd626fc327cb82',1,'Groupe']]],
   ['loadmodificationfromressource',['loadModificationFromRessource',['../class_e_d_t.html#a948d11bea42951fbf6e22929caf0be7b',1,'EDT']]],
   ['loadstatusfromressource',['loadStatusFromRessource',['../class_personne.html#a543bc428d6844c9fcfc36a24112f5043',1,'Personne']]],
   ['loadstudentfromressource',['loadStudentFromRessource',['../class_groupe.html#aed5526ddb3719ad9628186d62db94c2e',1,'Groupe']]],
-  ['loadtimetablefromressource',['loadTimetableFromRessource',['../class_matiere.html#ad5eef4807be1caf5be27f650fa0b82a8',1,'Matiere']]],
   ['login',['logIn',['../class_utilisateur.html#abe973775a4a08896c8ea708b02ce275f',1,'Utilisateur']]],
   ['logout',['logOut',['../class_utilisateur.html#a9dbc3f3370308b59b039afc9a48381b3',1,'Utilisateur']]]
 ];
